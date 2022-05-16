0b6c284bdd678b5d2c0894c2da502b780498dcd5
igoryan <nkulinigor1984@icloud.com>
2022-05-16 22:45:39 +0200

    task_02

---
+++ task_02/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать одномерный массив, заполненный случайными числами. 
+// Найти сумму элементов, стоящих на нечётных позициях.
+
+int size = 11;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 99);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    int sum = 0;
+    Console.WriteLine();
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(arr[i] + " ");
+        if (i % 2 > 0) sum += arr[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма элементов на нечётной позиции равна {sum} ");
+    Console.WriteLine();
+}
+
+PrintArray(FillArray(size));
