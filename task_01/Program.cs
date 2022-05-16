e726967b19bdf99fb52d9b02b3ea4f19f5d098b4
igoryan <nkulinigor1984@icloud.com>
2022-05-16 22:30:58 +0200

    task_01

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,2 +1,27 @@
-﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
 
+int size = 11;
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+    return array;
+}
+void PrintResult(int[] array)
+{
+    int even = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+        if (array[i] % 2 == 0) even++;
+        
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Количество четных чисел - {even}");
+    Console.WriteLine();
+}
+PrintResult(FillArray(size));
